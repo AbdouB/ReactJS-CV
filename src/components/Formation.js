ba3ac4a22072b558168ca57ec12125aa8a2edd74
Abdelilah Benhima <abdelilahbenhima@gmail.com>
2016-12-27 22:03:46 +0000

    added formation data and component

---
+++ src/components/Formation.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import formations from '../data/formations';
+
+export default class Formation extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            anneeFormation: "_17"
+        };
+    }
+
+
+    changeFormation(e){
+        this.setState({anneeFormation: e.target.value});
+    }
+    
+    
+    render(){
+        const annee = this.state.anneeFormation;
+        console.log(annee);
+        return(
+            <div className="formation w3-panel w3-card-4">
+                <h4>Formations: </h4>
+                <div className="row">
+                    <div className="col-md-3 col-sm-6 col-xs-12">
+                        <select className="form-control" onChange={this.changeFormation.bind(this)}>
+                            <option value="_17">2016/2017</option>
+                            <option value="_16">2014/2016</option>
+                            <option value="_14">2013/2014</option>
+                            <option value="_13">2012/2013</option>
+                        </select>              
+                    </div>
+                    <div className="col-md-9 col-sm-6 text-center"><p>{formations[annee]}</p></div> 
+                </div>
+            </div>
+             
+        )
+    }
+}
---
+++ src/data/formations.js
@@ -0,0 +1,11 @@
+
+const formations ={
+
+        '_17': 'Licence Professionnelle Développement en Applications Web et Innovation Numérique',
+        '_16': 'Diplôme Technicien Spécialisé en Développement Informatique à l’Ecole Française d’Enseignement Technique (EFET) ',
+        '_14': '1ère année en Faculté des Sciences Juridiques et Economiques Sociales Ain Sebâa.',
+        '_13': 'Baccalauréat en sciences économiques (Lycée Ibno mandour).'
+        
+}
+
+export default formations;
