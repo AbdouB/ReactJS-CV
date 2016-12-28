59cdd62f4132ee80ba467827eda0cfb93e20901c
Abdelilah Benhima <abdelilahbenhima@gmail.com>
2016-12-28 15:36:28 +0000

    updated main layout  to include Experience component

--- src/app-client.js
+++ src/app-client.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Profil from './components/Profil';
 import Formation from './components/Formation';
+import Experience from './components/Experience';
 
 window.onload = () => {
     ReactDOM.render
     (
         <div className="container">
             <Profil /> 
-            <Formation />        
+            <Formation /> 
+            <Experience />       
         </div>              
         , document.getElementById('app')
     );
