411d3dc65f944396c0a3c7f4a4934373757ed0e9
Abdelilah Benhima <abdelilahbenhima@gmail.com>
2016-12-28 15:35:01 +0000

    Added experience Data and Component

---
+++ src/components/Experience.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import experiences from '../data/experiences';
+
+export default class Experience extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            id: "cosumar"
+        };
+    }
+
+    changeExperience(e){
+        console.log(e.target.value);
+        this.setState({id: e.target.value})
+    }
+
+    render(){
+        
+        const id = this.state.id;
+        console.log(id);
+        console.log(experiences[id].titre);
+        return(
+            <div className="experience w3-panel w3-card-4">
+                <h4><span className="glyphicon glyphicon-briefcase" aria-hidden="true"></span>Experiences: </h4>
+                <div className="row">
+                    <div className="col-md-2 col-sm-4">
+                        <select className="form-control" onChange={this.changeExperience.bind(this)}>
+                            <option value="cosumar">Cosumar</option>
+                            <option value="pfe">Projet de fin d’étude</option>
+                        </select>              
+                    </div>
+                    <div className="col-md-10 col-sm-8 text-center">
+                        <h3>{experiences[id].titre}</h3><br/>
+                        {experiences[id].projets.map((projet, i) =>
+                            <div>
+                                <h5>-{projet.titre}</h5>
+                                 <span><img className="icon" src="img/github-reversed.svg"/></span>
+                                 <a href={"'"+projet.github+"'"}>Source code</a>
+                                <hr />
+                            </div>
+                        )}
+                    </div> 
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/data/experiences.js
@@ -0,0 +1,26 @@
+const experiences = {
+
+    'cosumar': {
+        'duree': 'Juillet 2015 ',
+        'titre': 'Stage au sein de la Compagnie Sucrière Marocaine de Raffinage (COSUMAR)',
+        'projets': [{
+            'titre': 'Conception et développement d’une application de gestion des incidents (J2EE/Play Framework/MySQL).',
+            'github': 'https://github.com/AbdouB/Gestion-des-incidents'
+        }, {
+            'titre': 'Conception et développement d’une application de gestion de stock informatique (Java SE).',
+            'github': 'https://github.com/AbdouB/gestion_stock_informatique'
+        }]
+    },
+
+    'pfe': {
+        'duree': 'Mai - Juillet 2016 ',
+        'titre': 'Projet de fin d’étude',
+        'projets': [{
+            'titre': 'Conception et développement d’une application web de gestion des locations des voitures (JSP/Servlets).',
+            'github': 'https://github.com/AbdouB/Gestion_location_voitures'
+        }]
+    }
+
+}
+
+export default experiences;
